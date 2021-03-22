e52ae0816802d76066d28614aafc1a6b124e0d1b
Moyse Murray <moysemurray@gmail.com>
2021-03-22 14:15:08 -0400

    Initial Commit

---
+++ ScribbleWorldTest/ScribbleWorldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleWorldTestApp.swift
+//  ScribbleWorldTest
+//
+//  Created by Franny Moyse on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleWorldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
